6db7a415fa34deb496671a59f8a01d3c6b4f21c6
weixin_global <315759265_global@qq.com>
2020-12-07 13:31:51 +0800

    11

---
+++ jd-meeting/jd-meeting-admin/src/main/java/com/qfjy/JdMeetingAdminApplication.java
@@ -0,0 +1,13 @@
+package com.qfjy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdMeetingAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JdMeetingAdminApplication.class, args);
+    }
+
+}
