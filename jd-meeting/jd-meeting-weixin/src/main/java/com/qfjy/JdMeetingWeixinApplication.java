27ae45ece18b300092719da368c7eb4ed3d36968
weixin_global <315759265_global@qq.com>
2020-12-07 13:43:22 +0800

    22

--- jd-meeting/jd-meeting-weixin/src/main/java/com/qfjy/JdMeetingWeixinApplication.java
+++ jd-meeting/jd-meeting-weixin/src/main/java/com/qfjy/JdMeetingWeixinApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JdMeetingWeixinApplication {
-
+//111
     public static void main(String[] args) {
         SpringApplication.run(JdMeetingWeixinApplication.class, args);
     }
